b0074a4e3ef8aae85d06092bfa38e04b0d72fd89
Denwa <ahgproxy@gmail.com>
2011-07-08 15:01:24 -0700

    Edited st.js via GitHub

--- st.js
+++ st.js
@@ -17,7 +17,8 @@ var word_filters = [ // Filtered words
     {pat : /everyone/ig,        new : 'everynyan'},
     {pat : /plan/ig,       new : 'keikaku'},
     {pat : /[^ ]*www.synchtube.com\/r\/E34fag/ig, new : 'Ban me for spammer!'},
-	{pat : /[^ ]*www.synchtube.com\/r\/4chanLive/ig, new : 'Ban me for spammer!'}
+    {pat : /[^ ]*www.synchtube.com\/r\/4chanLive/ig, new : 'Ban me for spammer!'}
+    {pat : /[^ ]*http://www.synchtube.com/r/Babby/ig, new : 'Ban me for spammer!'}
 ];
 
 // Convenience function for logging
