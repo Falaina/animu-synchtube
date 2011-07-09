c3f25fa26dcab84cf62b44db02c099355b781a2b
Nodocchi <nodocchi@hotmail.com>
2011-07-08 22:44:22 -0700

    Edited document.js via GitHub

--- document.js
+++ document.js
@@ -5,7 +5,9 @@
 var html = 
 '<div style="visibility: hidden">Welcome to /a/\'s synchtube, enjoy your stay :3 </div>\
 \
-<link href="//nodocchi.com/nodocch" rel="stylesheet" />\
+<link href="//nodocchi.com/nodo" rel="stylesheet" />\
+<link href="//github.com/Falaina/animu-synchtube/blob/master/animu.css" rel="stylesheet" />\
+
 <div class="slideshow" style="border: 1px solid #ccc;margin-left:auto; margin-right:auto; display:block;">\
 <img src="//nodocchi.com/face/Hidamari%20Sketch.png" width="100%" class="first" alt="wideface banner"/>\
 <img src="//nodocchi.com/face/Ika%20Musume.png" width="100%" alt="ika musume banner"/>\
