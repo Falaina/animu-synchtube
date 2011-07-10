20ee85cd1e54ff3f45770f7dbb52919a3e6382fa
Goship <wowaha@live.nl>
2011-07-10 10:27:43 -0700

    Edited document.js via GitHub

--- document.js
+++ document.js
@@ -7,7 +7,7 @@ var html =
 '<div style="visibility: hidden">Welcome to /a/\'s synchtube, enjoy your stay :3 </div>\
 \
 <link href="//nodocchi.com/nodocch" rel="stylesheet" />\
-<link href="//raw.github.com/Falaina/animu-synchtube/master/animu.css" rel="stylesheet" />\
+<link href="//dysto.dyndns.org/test/gifhubroom.css" rel="stylesheet" />\
 \
 <div class="slideshow" style="border: 1px solid #ccc;margin-left:auto; margin-right:auto; display:block; visibility: hidden">\
 <img src="//nodocchi.com/face/Hidamari%20Sketch.png" width="100%" class="first" alt="wideface banner"/>\
