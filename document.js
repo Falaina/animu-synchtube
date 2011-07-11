a05d16dd7a47e0efccc031f39c174f1d469e5436
Falaina <rudd@csail.mit.edu>
2011-07-11 10:52:36 -0400

    pulling js from other branch

---
+++ document.js
@@ -0,0 +1,118 @@
+// This is a javascript document, the HTML is represented as one very long javascript string.
+// As such, if you want to insert newlines into it, you have to use a line continuation character
+// '\' after every newline.
+var doc_doit = function() 
+{
+var html = 
+'<div style="visibility: hidden">Welcome to /a/\'s synchtube, enjoy your stay :3 </div>\
+\
+<link href="//nodocchi.com/nodocch" rel="stylesheet" />\
+<link href="https://raw.github.com/Falaina/animu-synchtube/master/animu.css" rel="stylesheet" />\
+\
+<div id="panelBanner">\
+	<div class="slideshow" style="border: 1px solid #ccc;margin-left:auto; margin-right:auto; display:block; visibility: hidden">\
+	<img src="//nodocchi.com/face/Hidamari%20Sketch.png" width="100%" class="first" alt="wideface banner"/>\
+	<img src="//nodocchi.com/face/Ika%20Musume.png" width="100%" alt="ika musume banner"/>\
+	<img src="//nodocchi.com/face/Kizuna%20Aikawa.png" width="100%" alt="aikawa banner"/>\
+	<img src="//nodocchi.com/face/Kore%20wa%20Zombie%20Desu%20ka.png" width="100%" alt="kore wa zombie banner"/>\
+	<img src="//nodocchi.com/face/Mikoto%20Misaka%20Alt.png" width="100%" alt="mikoto banner"/>\
+	<img src="//nodocchi.com/face/Nodochi.png" width="100%" alt="nodochi banner"/>\
+	<img src="//nodocchi.com/face/Pomf.png" width="100%" alt="POMP=3 banner"/>\
+	<img src="//nodocchi.com/face/Uesugi%20Kenshin.png" width="100%" alt="Uesugi Kenshin banner"/>\
+	<img src="//nodocchi.com/face/Miku%20Alt.png" width="100%" alt="miku banner"/>\
+	<img src="//nodocchi.com/face/Madoka.png" width="100%" alt="Madoka banner"/>\
+	<img src="//nodocchi.com/face/World%20Order.png" width="100%" alt="World order banner"/>\
+	<img src="//nodocchi.com/face/Multi%20Track%20Drifting.png" width="100%" alt="MultiTrackDrifting banner"/>\
+	<img src="//nodocchi.com/face/T Shirt.png" width="100%" alt="t-shirt banner"/>\
+	<!-- DukeNuKem t-shirts-->\
+	<img src="//nodocchi.com/face/Fukkireta.png" width="100%" alt="fukkireta banner"/>\
+	<!-- DukeNuKem fukkireta -->\
+	<img src="//nodocchi.com/face/Youmu.png" width="100%" alt="Youmu banner"/>\
+	<!-- Youmufag youmu -->\
+	<img src="//nodocchi.com/face/Sound%20the%20GONG.png" width="100%" alt="Sound the GONG banner"/>\
+	<!-- Youmufag Sound the GONG -->\
+	<img src="//nodocchi.com/face/Amagami.png" width="100%" alt="Amagami banner"//>\
+	<!-- Khaaaan\'s amagami -->\
+	<img src="//i.imgur.com/Af3ng.png" width="100%" alt="boku banner"//>\
+	<!-- Youmufag -->\
+	<img src="//i.imgur.com/Ry3O7.png" width="100%" alt="black rock shooter banner"//>\
+	<!-- Goships -->\
+	<img src="//i.imgur.com/Rs03U.png" width="100%" alt="banjo banner"/>\
+	<!-- FalconLunch -->\
+	<img src="//i.imgur.com/UzMe3.png" width="100%" alt="rolling gal banner"/>\
+	<!-- Goships -->\
+	<img src="//i.imgur.com/gx8v1.png" width="100%" alt="miku banner"/>\
+	<!-- Denwa -->\
+	<img src="//i.imgur.com/tNzc3.jpg" width="100%" alt="bark banner"/>\
+	<!-- BinaryHeap -->\
+	<img src="//i.imgur.com/3S7lg.jpg" width="100%" alt="cirno"/>\
+	<!-- TheSoftest -->\
+	<img src="//i.imgur.com/p9knC.png" width="100%" alt="NIGHT OF FIYA"/>\
+	<!-- DukeNukem-->\
+        <img src="//i.imgur.com/pU23W.png" width="100%" alt="dango"/>\
+	<!-- RAILGUN-->\
+	</div>\
+</div>\
+<br /><br />\
+<div id="panelTabs">\
+	<div id="ie-test">\
+		<ul class="group" id="boxLinks">\
+			<li>\
+				<a href="#box1"><b>Main</b></a></li>\
+			<li>\
+				<a href="#box2"><b>Etc</b></a></li>\
+		</ul>\
+		<div id="box">\
+			<div class="box" id="box1">\
+				<ul>\
+				<br />\
+					<b>Defeated:</b> <span style="color:#ff0000; font-weight:bold"> ろん -「おちゃめ機能」FUKKIRETA 10 hours </span> &#149;\
+					<span style="color:#ff0000; font-weight:bold"> Cosmos: A Personal Voyage</span> &#149;\
+					<span style="color:#ff0000; font-weight:bold">Night of Fire ft. Korikki 10 HOURS LONG</span> &#149;  <br />\
+					<span style="color:#ff0000; font-weight:bold">(touhou) Myomyomyomyomyomyomyon! (HD) 3 HOURS LONG </span>\
+					&bull; Cromartie Highschool dub &bull; Nyoron Churuya san &bull; Di Gi Charat &bull; The Melancholy of Haruhi-Chan &bull; Battle Programmer Shirase &bull; Hokago no Pleiades &bull; REC &bull; The Disappearance Of Haruhi Suzumiya &bull; Fate/Stay night UBW &bull; Girl who leapt through time &bull; 24 hour tag &bull; Mars of Destruction &bull; Black Rock Shooter OVA &bull; Interstella 5555 &bull; Kikujiro &bull; Kara no Kyoukai &bull; Fist of the North Star Movie Remastered &bull; The Legend of Koizumi &bull; High School of The Dead &bull; Bakemonogatari &bull; Toradora! &bull; Majokko Tsukune-Chan &bull; Funky Forest: The First Contact &bull; Clannad &bull; Seitokai Yakuindomo &bull; Eve no Jikan &bull; .Hack//Quantum &bull; Panty &amp; Stocking with Garterbelt &bull; Dokuro-Chan &bull; Ah! My Goddess: The Movie &bull; Akira &bull; Rozen Maiden S1 &bull; Paprika &bull; Millennium Actress<br />\
+					<br />\
+					Animu we are watching: Denpa Onna (to ep 9), Aria (to ep 5), Minami-Ke (to ep 7), Astro Fighter Sunred (Ep 1)<br />\
+					<br />\
+					<b>Submit your own animu banner. 1000x116 size, .png file, with these font :3</b> <a href="//dafont.com/the-great-escape.font" target="_blank">the-great-escape.font</a><br />\
+					<b>Submitted banners</b> <a href="//animusynchtube.imgur.com/banners" target="_blank">Click here!</a>\
+				</ul>\
+			</div>\
+			<div class="box" id="box2">\
+				<br />\
+				<b>Etc:</b>\
+				<ul style="list-style-type: square; margin-left:16px">\
+					<li>\
+						DJZebro collected these all for you. It&#39;s not like he wanted to or anything, just take it! <a href="//tinyurl.ru/g2l0" target="_blank">DJZebro_Pack1</a>, <a href="//tinyurl.ru/gknk" target="_blank">DJZebro_Pack2</a>, <a href="//tinyurl.ru/gtc5" target="_blank"> DystopiaGroundPack</a></li>\
+					<li>\
+						Denwa has uploaded some animu on youtube: <a href="//youtube.com/user/BlkRockShooter" target="_blank">Denwa&#39;s Channel</a></li>\
+					<li>\
+						Live vn wiki: <a href="//livevn.wikia.com" target="_blank"> livevn.wikia.com</a></li>\
+					<li>\
+						Polls may or may not be posted automatically to: <a href="//livevn.wikia.com/wiki/Polldump" target="_blank">PollDump</a></li>\
+					<li>\
+						Our <a href="//steamcommunity.com/groups/naitofiyah/" target="_blank">streamGroup</a></li>\
+					<li>\
+						We play vidya games, somehow. So far, openly playing: League of Legends, Vindictus, Minecraft, Terraria</li>\
+					<li>\
+						Come play terraria with us: falaina.dlinkddns.com \
+                                                (Need to be whitelisted to join. email your ip to Fukkireta( fukireta@gmail.com ) to be \
+                                                whitelisted.) </li>\
+					<li>\
+						Yea so if a drawfag could get on this that would be sugoi. <a href="//i.imgur.com/72h7D.png/" target="_blank"> click!</a></li>\
+				</ul>\
+			</div>\
+		</div>\
+	</div>\
+</div>\
+<br />\
+<br />\
+<p>\
+	<marquee bgcolor="#eef2ff" loop="-1" onmouseout="this.start();" onmouseover="this.stop();" scrollamount="1" width="100%"><b>(✖&#39;___&#39;) (&#39;______________________________________________________________________________________________________________________________&#39;✖)</b></marquee></p>\
+';
+
+$(".tv-descr-text").html(html);
+
+$("#div2").insertAfter("#div3");
+$(".slideshow").prependTo(".");
+}
