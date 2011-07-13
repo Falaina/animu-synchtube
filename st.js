693e7679d1c577e7b805b6523d02e8687617206d
Falaina <rudd@csail.mit.edu>
2011-07-12 20:42:37 -0700

    [DEPLOY HOOK]

--- st.js
+++ st.js
@@ -19,8 +19,13 @@ var word_filters = [ // Filtered words
     {pat : /everyone/ig,     target : 'everynyan'},
     {pat : /plan/ig,         target : 'keikaku'},
     {pat : /(QB)/ig,         target : '／人◕ ‿‿ ◕人＼'},
+    {pat : /(kitaa)/ig,       target : 'キタ━━━(゜∀゜)━━━!!!!!'},
+    {pat : /(bu-n)/ig,       target : '⊂二二二（　＾ω＾）二⊃'},
+    {pat : /(grimace)/ig,       target : '(╬ ಠ益ಠ)'},
+    {pat : /(simper)/ig,       target : '(￣ー￣)'},
     {pat : /(kyuubey|kyubey|kyubei)/ig,              target : 'coobie'},
     {pat : /magica/ig,       target : 'meguca'}
+
 ];
 
 
