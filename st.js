08482ef56e9e549d632e9bf12336ad0876284da8
Goship <wowaha@live.nl>
2011-07-12 23:08:52 -0700

    jquery toggle container added

--- st.js
+++ st.js
@@ -160,5 +160,14 @@ var doit = function (){
             return false;
         });	
 	$(".slideshow").css("visibility", "visible");
+
+	$(".toggle_container").hide(); 
+	$(".open").show(); 
+
+	//Switch the "Open" and "Close" state per click then slide up/down (depending on open/close state)
+	$("h4.trigger").click(function(){
+		$(this).toggleClass("active").next().slideToggle("slow");
+		return false; //Prevent the browser jump to the link anchor
+	});
     });
 };
