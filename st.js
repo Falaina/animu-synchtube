fe9ae2e349ccf24672d7640298e17be0195db516
Denwa <ahgproxy@gmail.com>
2011-07-29 15:14:06 -0700

    Edited st.js via GitHub

--- st.js
+++ st.js
@@ -155,7 +155,7 @@ var animu_synchtube = (function() {
     };
 
     var str_Alert = [
-	{pat  : /[^ ]*www.synchtube.com\/r\/([^ ]+)/ig, target: '[censored: $1]'},
+	{pat  : /[^ ]*synchtube.com\/r\/([^ ]+)/ig, target: '[censored: $1]'},
         {pat  : /[^ ]*synchtu.be\/([^ ]+)/ig, target: '[censored: $1]'}
     ];
 
