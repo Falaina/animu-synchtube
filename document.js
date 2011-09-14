527803a383604c493d86d207daaf359e5bea23b3
Goship <wowaha@live.nl>
2011-09-15 01:10:25 +0300

    Edited document.js via GitHub

--- document.js
+++ document.js
@@ -7,8 +7,7 @@ var doc_doit = function()
 var html = 
 '<link href="//dysto.dyndns.org/test/cssbanner/synchbanner2.css" rel="stylesheet" />'+
 '<link href="//dysto.dyndns.org/synchtube/special.css" rel="stylesheet" />'+
-'<link href="//dysto.dyndns.org/synchtube/steinsTheme.css" rel="stylesheet" />'+
-'<div class="customTheme"></div>' +
+'<div class="customTheme"><link href="//dysto.dyndns.org/synchtube/steinsTheme.css" rel="stylesheet" /></div>' +
 '<div id="panelTabs">' +
 '	<div id="ie-test">' +
 '		<ul class="group" id="boxLinks">' +
