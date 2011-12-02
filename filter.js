c3d0573dab9d24581c18781623dc42732fbbb53c
Falaina <rudd@csail.mit.edu>
2011-12-02 08:47:10 -0500

    Update filter.js

--- filter.js
+++ filter.js
@@ -21,6 +21,7 @@ if(!Message_one.fn.render.$instrumented) {
     else {
       var urlified = (helpers && helpers.linkify && helpers.linkify(this.item.msg)) || this.item.msg;
       if(urlified === this.item.msg) {
+      	console.log(this.item);
 	for(var i=0; i < word_filters.length; i++) {
 	  this.item.msg = this.item.msg.replace(word_filters[i].pat, word_filters[i].target);
 	}
