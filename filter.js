c323d0d10721cdbe312bc2e0d2e4e404d72fa06a
Falaina <rudd@csail.mit.edu>
2011-12-21 00:54:54 -0500

    Update filter.js

--- filter.js
+++ filter.js
@@ -46,8 +46,8 @@ String.prototype.hashCode = function(){
 
 var word_filters = [
   //{pat : /(b(i|o)n(a|e)r(y|u)(-*heap|hump){0,1})/ig,		target : '$1-niichan'},      
-  {pat : /(b(i|o)n(a|e)r(y|u)(-*heap|hump))/ig,		target : 'yuruyuri'},        
-  {pat : /(b(i|o)n(a|e)r(y|u))/ig,		target : 'yuru'},        
+  {//pat : /(b(i|o)n(a|e)r(y|u)(-*heap|hump))/ig,		target : 'yuruyuri'},        
+  //{pat : /(b(i|o)n(a|e)r(y|u))/ig,		target : 'yuru'},        
   //{pat : /fuk(k){0,1}i(reta){0,1}/ig,		target : 'chiruno'},      
   //{pat : /(@_+@|~_+~)/g,                  target : 'XD'},
   //{pat : /\b(:3)\b/g,                  target : 'xiox :3'},
