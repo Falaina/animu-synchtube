78fcae5e1519310546079a15bdf51f571f6334ad
Falaina <rudd@csail.mit.edu>
2011-12-30 14:15:04 -0500

    Update filter.js

--- filter.js
+++ filter.js
@@ -48,7 +48,7 @@ var word_filters = [
   {pat : /(b(i|o)n(a|e)r(y|u)(-*heap|hump){0,1})/ig,		target : '$1-neesama'},      
   //{pat : /(b(i|o)n(a|e)r(y|u)(-*heap|hump))/ig,		target : 'yuruyuri'},        
   //{pat : /(b(i|o)n(a|e)r(y|u))/ig,		target : 'yuru'},        
-  //{pat : /fuk(k){0,1}i(reta){0,1}/ig,		target : 'chiruno'},      
+  {pat : /fuk(k){0,1}i(reta){0,1}/ig,		target : 'POMF =3'},      
   //{pat : /(@_+@|~_+~)/g,                  target : 'XD'},
   //{pat : /\b(:3)\b/g,                  target : 'xiox :3'},
   //{pat : /\b(u_+u)\b/g,                  target : '^_^'},
