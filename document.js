765912c83cec0612fee7f06cc12502f427fe43ca
Goship <wowaha@live.nl>
2011-12-30 16:16:49 -0800

    firework option2

--- document.js
+++ document.js
@@ -312,7 +312,7 @@ $('#playlist .items li').each(function() {var id = $(this).attr('id').replace('m
 });
 
 //fireworks.js
-$("#stagetools").append("<div class='fireworks'>FIRE</div>");
+//$("#stagetools").append("<div class='fireworks'>FIRE</div>");
 $.getScript('http://dysto.dyndns.org/synchtube/script/fireworks.js', function(){
 	console.log("enter function");
 	$("#stagetools div.fireworks").live("click", function() {
