f34b900b098fa2e9444ace28c7e4815d304a34e0
Falaina <rudd@csail.mit.edu>
2012-01-22 09:24:37 -0500

    Update document.js

--- document.js
+++ document.js
@@ -386,7 +386,6 @@ $.getScript('http://dysto.dyndns.org/synchtube/script/fireworks.js', function(){
     });
 	$(".banner" ).fadeIn('slow');
 });
-};
 
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-28544703-1']);
@@ -397,3 +396,6 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+
+};
