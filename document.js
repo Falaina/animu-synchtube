25e051c49260f4351cafae7d7291f53ffbaef020
Falaina <rudd@csail.mit.edu>
2012-01-25 12:34:18 -0500

    analytics

--- document.js
+++ document.js
@@ -277,6 +277,17 @@ var html =
 
 $(" #description .description").html(html);
 
+_gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-28544703-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
 // Set up banner and infobox transitions
 $('.box').hide();
 $('ul.group li:first').addClass('active').show();
@@ -387,16 +398,4 @@ $.getScript('http://dysto.dyndns.org/synchtube/script/fireworks.js', function(){
     });
 	$(".banner" ).fadeIn('slow');
 });
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-28544703-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
-
 };
