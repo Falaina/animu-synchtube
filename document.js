328c100a72067344fe2fcc2baff29d99660e1ff7
Goship <wowaha@live.nl>
2012-01-26 01:49:35 +0100

    Update document.js

--- document.js
+++ document.js
@@ -275,6 +275,16 @@ var html =
 ' ';
 
 $(" #description .description").html(html);
+ 
+_gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-28544703-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 
 // Set up banner and infobox transitions
 $('.box').hide();
@@ -394,15 +404,6 @@ $.getScript('http://dysto.dyndns.org/synchtube/script/fireworks.js', function(){
 	$(".banner" ).fadeIn('slow');
 });
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-28544703-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
 
 
 };
