8e01f6b572e144d10c6dc7d2effcbcc71af15ff8
Falaina <rudd@csail.mit.edu>
2012-03-03 08:57:21 -0500

    Save filtering settings.

--- document.js
+++ document.js
@@ -248,7 +248,8 @@ var html =
 '					<ul style="list-style-type: square; margin-left:16px">' +
 ' 						<li class="showFullList"><a href="#">Show full video list</a></li>'+ 
 ' 						<li class="linkify"><a href="#">Linkify playlist</a></li>'+ 
-' 						<li class="disableFilters"><a href="#">Disable Wordfilters (not retroactive)</a></li>'+ 
+' 						<li class="disableFilters"><a href="#">Disable Wordfilters (not retroactive). Persists between sessions.</a></li>'+ 
+' 						<li class="enableFilters"><a href="#">Enable Wordfilters (retroactive). Persists between sessions.</a></li>'+ 
 ' 					</ul>'+ 
 '			</div>' +
 '			<div class="box" id="box5" >' +
@@ -397,6 +398,12 @@ $(".showFullList").click(function(){
 });	
     //Turn the entire list into right/middle-clickable URLs
 $(".disableFilters").click(function(){word_filters = {};Cookie.createCookie("r_animu_disable_filters", "true");});
+$(".enableFilters").click(function()
+	Cookie.eraseCookie("r_animu_disable_filters");
+	setupFilters();
+	Message && Message.clear();
+	window.sp && window.sp.messages && window.sp.messages.addAll();		
+});
 $(".linkify").click(function() {
 $('#playlist .items li').each(function() {var id = $(this).attr('id').replace('media_', ''); var vid = Media.records[id]; if(vid.mtype === 'yt') {var url = 'http://www.youtube.com/watch?v='+vid.mid;console.log(url); var title = $(".title", this).html(); title = '<a target="_blank" class="play title" href="'+url+'">'+title+'</a>'; console.log(title); $(".title", this).html(title);}})
 });
