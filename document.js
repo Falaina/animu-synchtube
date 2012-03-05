ff4ebed74fdab80ae137642e77debd28cd517a21
Falaina <rudd@csail.mit.edu>
2012-03-05 10:41:03 -0500

    Remove linkify playlist

--- document.js
+++ document.js
@@ -252,7 +252,6 @@ var html =
 '					<b>Other Settings</b>' +
 '					<ul style="list-style-type: square; margin-left:16px">' +
 ' 						<li class="showFullList"><a href="#">Show full video list</a></li>'+ 
-' 						<li class="linkify"><a href="#">Linkify playlist</a></li>'+ 
 ' 						<li class="disableFilters"><a href="#">Disable Wordfilters. Persists between sessions.</a></li>'+ 
 ' 						<li class="enableFilters"><a href="#">Enable Wordfilters. Persists between sessions.</a></li>'+ 
 ' 					</ul>'+ 
