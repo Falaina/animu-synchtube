6391d7ea72c99f6fd80809120f75fbaa7c772178
Desuwa <Desuwaifu@gmail.com>
2012-07-30 21:56:39 -0300

    Update document.js

--- document.js
+++ document.js
@@ -1,6 +1,26 @@
 // This is a javascript document, the HTML is represented as one very long javascript string.
 // as such every line must be a valid string and appended to the main html string.
 
+function sanitize(s) {
+    return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
+}
+function sanitizeMedia(m) {
+    for(field in m) {
+        if(typeof(m[field]) === 'string') {
+            val = m[field]
+            console.log("Sanitizing %s", val);
+            m[field] = sanitize(val);
+        }
+    }        
+}
+
+Media.create = _.wrap(Media.create, function() {
+    fn = arguments[0];
+    J  = arguments[1];
+    sanitizeMedia(J);
+    return fn.apply(Media, _.rest(arguments));
+});
+
 var doc_doit = function() 
 {
 //$("#st-descr" ).prepend('<div class="neon">お誕生日おめでとう /a/\'s Synchtube <br /> This Room turns 1 year old!</div>');	
